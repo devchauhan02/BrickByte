365615e33aad27a9230f49de684485c45441b1da
Devashish Chauhan <a8265999139@gmail.com>
2025-04-25 15:39:27 +0530

    lets get started with the realState project

---
+++ server/index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+app.use(cookieParser());
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+}
+);
+
+app.listen(PORT, () => {   
+
+    console.log(`Server is running on port ${PORT}`);
+});
